3385948faa40047c7409eaaf531c29bda73bc89c
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2026-03-02 17:24:00 +0700

    Formatting

--- backend/src/env.ts
+++ backend/src/env.ts
@@ -4,5 +4,5 @@ import { loadEnvFile } from "process";
  * Loads environment variables from a `.env` file based on the current `NODE_ENV` value.
  */
 export function loadEnvironmentVariables() {
-    loadEnvFile(`.env.${process.env.NODE_ENV ?? "development"}`)
+    loadEnvFile(`.env.${process.env.NODE_ENV ?? "development"}`);
 }
