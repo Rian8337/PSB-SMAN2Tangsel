8906e73160b24aefe03871b3feddeea12fca8a75
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2026-03-16 22:20:15 +0700

    Add `ScheduleDTO` for schedule transfer between backend and frontend

--- packages/shared/src/types/schedules.ts
+++ packages/shared/src/types/schedules.ts
@@ -2,11 +2,23 @@
  * The days of the week for schedules.
  */
 export enum ScheduleDay {
-    sunday,
     monday,
     tuesday,
     wednesday,
     thursday,
     friday,
-    saturday,
+}
+
+/**
+ * Schedule data transferred between frontend and backend.
+ */
+export interface ScheduleDTO {
+    readonly id: number;
+    readonly day: ScheduleDay;
+    readonly startTime: number;
+    readonly endTime: number;
+    readonly subject: {
+        readonly code: string;
+        readonly name: string;
+    };
 }
