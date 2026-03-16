ba0f2de0e4a6b69429283b2b3403bf210c98ebbc
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2026-03-16 17:05:12 +0700

    Allow silent loading of environment variables

--- backend/src/env.ts
+++ backend/src/env.ts
@@ -4,8 +4,10 @@ import { loadEnvFile } from "process";
  * Loads environment variables from a `.env` file based on the current `NODE_ENV` value.
  *
  * If this is called in a Continuous Integration environment, no file will be loaded.
+ *
+ * @param silent Whether a missing `.env` file will be logged as a warning. Defaults to `false`.
  */
-export function loadEnvironmentVariables() {
+export function loadEnvironmentVariables(silent = false) {
     if (!process.env.CI) {
         const nodeEnv = process.env.NODE_ENV ?? "development";
 
@@ -13,9 +15,11 @@ export function loadEnvironmentVariables() {
             loadEnvFile(`.env.${nodeEnv}`);
         } catch {
             // Gracefully ignore missing files so the server can boot using OS variables
-            console.warn(
-                `[Env] No .env.${nodeEnv} file found. Relying on system environment variables.`,
-            );
+            if (!silent) {
+                console.warn(
+                    `[Env] No .env.${nodeEnv} file found. Relying on system environment variables.`,
+                );
+            }
         }
     }
 }
