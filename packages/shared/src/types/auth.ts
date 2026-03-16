16ec65c077fb4d76a214992e0ee5c6a4abd98728
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2026-03-16 22:35:59 +0700

    Add methods to check for failed logins

--- packages/shared/src/types/auth.ts
+++ packages/shared/src/types/auth.ts
@@ -1,8 +1,46 @@
 import { UserRole } from "./users";
 
 /**
+ * Represents a successful login response.
+ */
+export interface SuccessfulLoginResponse {
+    readonly id: number;
+    readonly name: string;
+    readonly role: UserRole;
+}
+
+/**
+ * Represents a failed login response.
+ */
+export interface FailedLoginResponse {
+    readonly error: string;
+}
+
+/**
  * The response that is given when a user logs in or checks for their identity.
  */
-export type LoginResponseBody =
-    | { id: number; name: string; role: UserRole }
-    | { error: string };
+export type LoginResponseBody = SuccessfulLoginResponse | FailedLoginResponse;
+
+/**
+ * Determines whether a login response body represents a successful login.
+ *
+ * @param body The login response body to check.
+ * @returns `true` if the body represents a successful login, `false` otherwise.
+ */
+export function isSuccessfulLogin(
+    body: LoginResponseBody,
+): body is SuccessfulLoginResponse {
+    return (body as SuccessfulLoginResponse | undefined)?.id !== undefined;
+}
+
+/**
+ * Determines whether a login response body represents a failed login.
+ *
+ * @param body The login response body to check.
+ * @returns `true` if the body represents a failed login, `false` otherwise.
+ */
+export function isFailedLogin(
+    body: LoginResponseBody,
+): body is FailedLoginResponse {
+    return (body as FailedLoginResponse | undefined)?.error !== undefined;
+}
