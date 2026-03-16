be997ae5f891fc7f676fc73bb12597b7a58414b4
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2026-03-16 23:14:03 +0700

    Add server-side factories of `APIClient`s

---
+++ frontend/src/api/server/index.ts
@@ -0,0 +1,32 @@
+import { getLocale } from "next-intl/server";
+import { IAuthAPIClient } from "../IAuthAPIClient";
+import { AuthAPIClient } from "../AuthAPIClient";
+import { Locale } from "next-intl";
+import { IScheduleAPIClient } from "../IScheduleAPIClient";
+import { ScheduleAPIClient } from "../ScheduleAPIClient";
+
+/**
+ * Server-side factory to retrieve an {@link IAuthAPIClient}.
+ *
+ * @param locale Optional locale to initialize the {@link IAuthAPIClient} with. If not provided, it will be
+ * retrieved using {@link getLocale}.
+ * @returns An {@link IAuthAPIClient} initialized with the specified or retrieved locale.
+ */
+export async function getServerAuthApiClient(
+    locale?: Locale,
+): Promise<IAuthAPIClient> {
+    return new AuthAPIClient(locale ?? (await getLocale()));
+}
+
+/**
+ * Server-side factory to retrieve an {@link IScheduleAPIClient}.
+ *
+ * @param locale Optional locale to initialize the {@link IScheduleAPIClient} with. If not provided, it will be
+ * retrieved using {@link getLocale}.
+ * @returns An {@link IScheduleAPIClient} initialized with the specified or retrieved locale.
+ */
+export async function getServerScheduleApiClient(
+    locale?: Locale,
+): Promise<IScheduleAPIClient> {
+    return new ScheduleAPIClient(locale ?? (await getLocale()));
+}
