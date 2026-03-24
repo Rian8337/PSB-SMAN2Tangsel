8935e3ae314db48a3e8bd983f06db43633e40351
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2026-03-24 16:09:47 +0700

    Add metadata for user account settings page

--- frontend/src/app/[locale]/(protected)/(main)/settings/page.tsx
+++ frontend/src/app/[locale]/(protected)/(main)/settings/page.tsx
@@ -1,4 +1,16 @@
 import { AccountSettings } from "@/components/settings/AccountSettings";
+import { Locale } from "next-intl";
+import { getTranslations } from "next-intl/server";
+
+export async function generateMetadata({
+    params: { locale },
+}: {
+    params: { locale: Locale };
+}) {
+    const t = await getTranslations({ locale, namespace: "AccountSettings" });
+
+    return { title: t("title") };
+}
 
 export default function AccountSettingsPage() {
     return <AccountSettings />;
