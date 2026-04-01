fd4d4038ec956dddec25c8ea5a1c400d804976d1
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2026-04-01 18:38:14 +0800

    Refactor backend-side login utility to find from users list

--- backend/tests/utils/auth.ts
+++ backend/tests/utils/auth.ts
@@ -1,4 +1,5 @@
 import { seededPrimaryData } from "@psb/shared/tests";
+import { UserRole } from "@psb/shared/types";
 import TestAgent from "supertest/lib/agent";
 
 const loginEndpoint = "/auth/login";
@@ -7,50 +8,48 @@ const testPassword = "password123";
 /**
  * Logins a test agent as a student.
  *
- * This uses the first student from {@link seededPrimaryData.students} to perform the login.
+ * This uses the first student from {@link seededPrimaryData.users} to perform the login.
  *
  * @param agent The test agent to login.
  * @returns The response from the login endpoint.
  */
 export function loginStudent(agent: TestAgent) {
-    const student = seededPrimaryData.students[0];
-
-    return agent.post(loginEndpoint).send({
-        id: student.nisn,
-        password: testPassword,
-    });
+    return loginUser(agent, UserRole.student);
 }
 
 /**
  * Logins a test agent as a teacher.
  *
- * This uses the first teacher from {@link seededPrimaryData.teachers} to perform the login.
+ * This uses the first teacher from {@link seededPrimaryData.users} to perform the login.
  *
  * @param agent The test agent to login.
  * @returns The response from the login endpoint.
  */
 export function loginTeacher(agent: TestAgent) {
-    const teacher = seededPrimaryData.teachers[0];
-
-    return agent.post(loginEndpoint).send({
-        id: teacher.staffId.toString(),
-        password: testPassword,
-    });
+    return loginUser(agent, UserRole.teacher);
 }
 
 /**
  * Logins a test agent as an administrator.
  *
- * This uses the first administrator from {@link seededPrimaryData.administrators} to perform the login.
+ * This uses the first administrator from {@link seededPrimaryData.users} to perform the login.
  *
  * @param agent The test agent to login.
  * @returns The response from the login endpoint.
  */
 export function loginAdministrator(agent: TestAgent) {
-    const admin = seededPrimaryData.administrators[0];
+    return loginUser(agent, UserRole.administrator);
+}
+
+function loginUser(agent: TestAgent, role: UserRole) {
+    const user = seededPrimaryData.users.find((u) => u.role === role);
+
+    if (!user) {
+        throw new Error(`No user found with role ${role.toString()}`);
+    }
 
     return agent.post(loginEndpoint).send({
-        id: admin.staffId.toString(),
+        id: user.identifier,
         password: testPassword,
     });
 }
