4f7091fecd9846d8dc7a4d12168ca477ca2c48c3
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2026-04-07 21:49:38 +0800

    Add composite index for active and name, and change `active` column type

--- packages/shared/src/database/schema/subjects.ts
+++ packages/shared/src/database/schema/subjects.ts
@@ -1,34 +1,39 @@
 import { relations } from "drizzle-orm";
 import { boolean, int, mysqlTable, varchar } from "drizzle-orm/mysql-core";
 import { classSubjects } from "./classSubjects";
+import { index } from "drizzle-orm/mysql-core";
 
 /**
  * The subject table.
  */
-export const subjects = mysqlTable("subject", {
-    /**
-     * The system-issued identification number of the subject.
-     */
-    id: int().autoincrement().primaryKey(),
+export const subjects = mysqlTable(
+    "subject",
+    {
+        /**
+         * The system-issued identification number of the subject.
+         */
+        id: int().autoincrement().primaryKey(),
 
-    /**
-     * The code of the subject.
-     */
-    code: varchar({ length: 15 }).unique("code_unique").notNull(),
+        /**
+         * The code of the subject.
+         */
+        code: varchar({ length: 15 }).unique("code_unique").notNull(),
 
-    /**
-     * The name of the subject.
-     */
-    name: varchar({ length: 50 }).notNull(),
+        /**
+         * The name of the subject.
+         */
+        name: varchar({ length: 50 }).notNull(),
 
-    /**
-     * Whether the subject is active.
-     *
-     * Inactive subjects will not appear in the subject selection dropdown when assigning subjects to a class, but will
-     * still be visible in the subject management page.
-     */
-    active: boolean().$type<true | null>().default(true),
-});
+        /**
+         * Whether the subject is active.
+         *
+         * Inactive subjects will not appear in the subject selection dropdown when assigning subjects to a class, but will
+         * still be visible in the subject management page.
+         */
+        active: boolean().notNull().default(true),
+    },
+    (table) => [index("active_name_idx").on(table.active, table.name)],
+);
 
 /**
  * Relations for the {@link subjects} table.
