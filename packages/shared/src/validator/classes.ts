9bc3e04fdb798b0566f6981aa1f521210bb098ef
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2026-04-09 10:42:33 +0800

    Separate class name validator

--- packages/shared/src/validator/classes.ts
+++ packages/shared/src/validator/classes.ts
@@ -4,13 +4,18 @@ import { classes } from "../database/schema";
 import { validSemesterSchema, validSessionSchema } from "./sessions";
 
 /**
+ * A Zod schema for validating class names.
+ */
+export const validClassNameSchema = z
+    .string()
+    .min(1, "Class name is required")
+    .max(50, "Class name must be at most 50 characters long");
+
+/**
  * A Zod schema for validating class insertion data.
  */
 export const insertClassSchema = createInsertSchema(classes, {
-    name: z
-        .string()
-        .min(1, "Class name is required")
-        .max(50, "Class name must be at most 50 characters long"),
+    name: validClassNameSchema,
     semester: validSemesterSchema,
     session: validSessionSchema,
-});
+}).omit({ id: true });
