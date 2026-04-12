1a4a397575a666d0e9eeafe069fe1850ab0f63dc
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2026-04-12 10:13:25 +0800

    Add integration test for `ClassSubjectController`

---
+++ backend/tests/integration/controllers/ClassSubjectController.int.test.ts
@@ -0,0 +1,311 @@
+import { seededPrimaryData } from "@psb/shared/tests";
+import { ClassSubjectAssignment, Subject } from "@psb/shared/types";
+import { app } from "@test/setup/app";
+import {
+    loginAdministrator,
+    loginStudent,
+    loginTeacher,
+    seeders,
+    testDbManager,
+} from "@test/utils";
+import request from "supertest";
+
+describe("ClassSubjectController (integration)", () => {
+    const teacher = seededPrimaryData.teachers[0];
+    const subjectAssigned = seededPrimaryData.subjects[0];
+    const subjectUnassigned = seededPrimaryData.subjects[1];
+
+    let classId: number;
+    let classSubjectId: number;
+
+    beforeAll(async () => {
+        const session = seededPrimaryData.sessions[0];
+
+        const clazz = await seeders.classes.seedOne({
+            name: "Test Class",
+            session: session.session,
+            semester: session.semester,
+        });
+
+        classId = clazz.id!;
+
+        const classSubject = await seeders.classSubjects.seedOne({
+            classId,
+            subjectId: subjectAssigned.id,
+            teacherId: teacher.userId,
+        });
+
+        classSubjectId = classSubject.id!;
+    });
+
+    afterAll(testDbManager.cleanupSecondaryTables);
+
+    describe("GET /class-subjects/:id/assigned", () => {
+        let endpoint: string;
+
+        beforeAll(() => {
+            endpoint = `/class-subjects/${classId.toString()}/assigned`;
+        });
+
+        it("should return 401 if requested without authentication", async () => {
+            const res = await request(app).get(endpoint);
+
+            expect(res.status).toBe(401);
+        });
+
+        describe("Student", () => {
+            const agent = request.agent(app);
+
+            beforeAll(async () => await loginStudent(agent));
+
+            it("should restrict access", async () => {
+                const res = await agent.get(endpoint);
+
+                expect(res.status).toBe(403);
+            });
+        });
+
+        describe("Teacher", () => {
+            const agent = request.agent(app);
+
+            beforeAll(async () => await loginTeacher(agent));
+
+            it("should restrict access", async () => {
+                const res = await agent.get(endpoint);
+
+                expect(res.status).toBe(403);
+            });
+        });
+
+        describe("Administrator", () => {
+            const agent = request.agent(app);
+
+            beforeAll(async () => await loginAdministrator(agent));
+
+            it("should return the assigned subjects", async () => {
+                const res = await agent.get(endpoint);
+                const body = res.body as ClassSubjectAssignment[];
+
+                expect(res.status).toBe(200);
+
+                expect(body).toBeInstanceOf(Array);
+                expect(body).toHaveLength(1);
+                expect(body[0].subject.id).toBe(subjectAssigned.id);
+            });
+        });
+    });
+
+    describe("GET /class-subjects/:id/unassigned", () => {
+        let endpoint: string;
+
+        beforeAll(() => {
+            endpoint = `/class-subjects/${classId.toString()}/unassigned`;
+        });
+
+        it("should return 401 if requested without authentication", async () => {
+            const res = await request(app).get(endpoint);
+
+            expect(res.status).toBe(401);
+        });
+
+        describe("Student", () => {
+            const agent = request.agent(app);
+
+            beforeAll(async () => await loginStudent(agent));
+
+            it("should restrict access", async () => {
+                const res = await agent.get(endpoint);
+
+                expect(res.status).toBe(403);
+            });
+        });
+
+        describe("Teacher", () => {
+            const agent = request.agent(app);
+
+            beforeAll(async () => await loginTeacher(agent));
+
+            it("should restrict access", async () => {
+                const res = await agent.get(endpoint);
+
+                expect(res.status).toBe(403);
+            });
+        });
+
+        describe("Administrator", () => {
+            const agent = request.agent(app);
+
+            beforeAll(async () => await loginAdministrator(agent));
+
+            it("should return the unassigned subjects", async () => {
+                const res = await agent.get(endpoint);
+                const body = res.body as Subject[];
+
+                expect(res.status).toBe(200);
+
+                expect(body).toBeInstanceOf(Array);
+                expect(body.some((s) => s.id === subjectUnassigned.id)).toBe(
+                    true,
+                );
+            });
+        });
+    });
+
+    describe("POST /class-subjects/:id", () => {
+        const payload = { subjectId: 2, teacherId: null };
+        let endpoint: string;
+
+        beforeAll(() => {
+            endpoint = `/class-subjects/${classId.toString()}`;
+        });
+
+        it("should return 401 if requested without authentication", async () => {
+            const res = await request(app).post(endpoint).send(payload);
+
+            expect(res.status).toBe(401);
+        });
+
+        describe("Student", () => {
+            const agent = request.agent(app);
+
+            beforeAll(async () => await loginStudent(agent));
+
+            it("should restrict access", async () => {
+                const res = await agent.post(endpoint).send(payload);
+
+                expect(res.status).toBe(403);
+            });
+        });
+
+        describe("Teacher", () => {
+            const agent = request.agent(app);
+
+            beforeAll(async () => await loginTeacher(agent));
+
+            it("should restrict access", async () => {
+                const res = await agent.post(endpoint).send(payload);
+
+                expect(res.status).toBe(403);
+            });
+        });
+
+        describe("Administrator", () => {
+            const agent = request.agent(app);
+
+            beforeAll(async () => await loginAdministrator(agent));
+
+            it("should assign the subject and return 201", async () => {
+                // Dynamically use the real unassigned subject ID
+                const res = await agent.post(endpoint).send({
+                    subjectId: subjectUnassigned.id,
+                    teacherId: null,
+                });
+
+                expect(res.status).toBe(201);
+            });
+        });
+    });
+
+    describe("PATCH /class-subjects/:id", () => {
+        const payload = { teacherId: null };
+        let endpoint: string;
+
+        beforeAll(() => {
+            endpoint = `/class-subjects/${classSubjectId.toString()}`;
+        });
+
+        it("should return 401 if requested without authentication", async () => {
+            const res = await request(app).patch(endpoint).send(payload);
+
+            expect(res.status).toBe(401);
+        });
+
+        describe("Student", () => {
+            const agent = request.agent(app);
+
+            beforeAll(async () => await loginStudent(agent));
+
+            it("should restrict access", async () => {
+                const res = await agent.patch(endpoint).send(payload);
+
+                expect(res.status).toBe(403);
+            });
+        });
+
+        describe("Teacher", () => {
+            const agent = request.agent(app);
+
+            beforeAll(async () => await loginTeacher(agent));
+
+            it("should restrict access", async () => {
+                const res = await agent.patch(endpoint).send(payload);
+
+                expect(res.status).toBe(403);
+            });
+        });
+
+        describe("Administrator", () => {
+            const agent = request.agent(app);
+
+            beforeAll(async () => await loginAdministrator(agent));
+
+            it("should update the assigned teacher and return 204", async () => {
+                const res = await agent.patch(endpoint).send({
+                    teacherId: teacher.userId,
+                });
+
+                expect(res.status).toBe(204);
+            });
+        });
+    });
+
+    describe("DELETE /class-subjects/:id", () => {
+        let endpoint: string;
+
+        beforeAll(() => {
+            endpoint = `/class-subjects/${classSubjectId.toString()}`;
+        });
+
+        it("should return 401 if requested without authentication", async () => {
+            const res = await request(app).delete(endpoint);
+
+            expect(res.status).toBe(401);
+        });
+
+        describe("Student", () => {
+            const agent = request.agent(app);
+
+            beforeAll(async () => await loginStudent(agent));
+
+            it("should restrict access", async () => {
+                const res = await agent.delete(endpoint);
+
+                expect(res.status).toBe(403);
+            });
+        });
+
+        describe("Teacher", () => {
+            const agent = request.agent(app);
+
+            beforeAll(async () => await loginTeacher(agent));
+
+            it("should restrict access", async () => {
+                const res = await agent.delete(endpoint);
+
+                expect(res.status).toBe(403);
+            });
+        });
+
+        describe("Administrator", () => {
+            const agent = request.agent(app);
+
+            beforeAll(async () => await loginAdministrator(agent));
+
+            it("should unassign the subject and return 204", async () => {
+                const res = await agent.delete(endpoint);
+
+                expect(res.status).toBe(204);
+            });
+        });
+    });
+});
